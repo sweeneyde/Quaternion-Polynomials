4ec186a9162a517b8fd23bfa516d05368dee85f5
Dennis Sweeney <sweeney.427@osu.edu>
2018-10-31 19:13:31 -0400

    Squashed commit to not stress out github with large files

--- main.py
+++ main.py
@@ -3,21 +3,15 @@ from polynomial_generators import *
 from quat_plotter import plot
 import csv
 from time import time
-from multiprocessing import Process, Manager, Pool
 
 ########## Choose which polynomials ################################
 
 polynomial_generator = all_signs_qp(degree=3)
 
-########## Solve them ##############################################
-#
-# def processCoefs(coefs, individual_solutions, class_solutions):
-#     inds, cls = quat_poly(*coefs, 1).roots()
-#     individual_solutions += inds
-#     class_solutions += cls
-
 def main():
     ########## Solve ##################################################
+
+    start_time = time()
 
     individual_solutions = []
     class_solutions= []
@@ -26,33 +20,7 @@ def main():
         individual_solutions += inds
         class_solutions += cls
 
-    # start_time = time()
-    #
-    # with Manager() as manager:
-    #     class_solutions = manager.list()
-    #     individual_solutions = manager.list()
-    #
-    #     processes = []
-    #     for i, coefs in enumerate(polynomial_generator):
-    #         #print(i)
-    #         p = Process(target=processCoefs,
-    #                     args=(coefs, individual_solutions, class_solutions)
-    #                     )
-    #         processes.append(p)
-    #         p.start()
-    #         if len(processes)>100:
-    #             # print('d', i)
-    #             processes[0].join()
-    #             del processes[0]
-    #
-    #     map(lambda p:p.join(), processes)
-    # #
-    # with Pool(processes=10) as pool:
-    #     class_solutions = []
-    #     individual_solutions = []
-    #     pool.map(processCoefs, zip(polynomial_generator), class_solutions, individual_solutions)
-
-    #print(f"Total run time: {time()-start_time} seconds")
+    print(f"Total run time: {time()-start_time} seconds")
 
     ########## Save and Print the solutions ###########################
 
@@ -67,6 +35,7 @@ def main():
 
     ########## Plot the solutions ######################################
 
+    # Here we can change which component gets represented as color
     plot([(a,b,c,d) for a,b,c,d in individual_solutions])
 
 
